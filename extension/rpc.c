9a21b7825e7c0879b1f6b20a92ec6e4a722af350
Simon Engledew <simon.engledew@osirium.com>
2020-07-14 08:46:39 +0100

    Tidy up send/receive code

--- extension/rpc.c
+++ extension/rpc.c
@@ -4,33 +4,22 @@
 #include <utils/guc.h>
 #include <utils/builtins.h>
 
-#include <string.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <sys/types.h>
 #include <sys/un.h>
-#include <sys/socket.h>
 #include <unistd.h>
 
 PG_MODULE_MAGIC;
 
-
-/* Function signatures */
 void _PG_init(void);
 void _PG_fini(void);
 
-
-/* Global variables */
 char * g_path;
 int g_timeout;
 
-/* Startup */
 void _PG_init(void)
 {
     DefineCustomStringVariable("rpc.path", "Hostname", NULL, &g_path, "/var/run/postgresql/rpc.sock", PGC_USERSET, GUC_NOT_IN_SAMPLE, NULL, NULL, NULL);
     DefineCustomIntVariable("rpc.timeout", "Timeout (Seconds)", NULL, &g_timeout, 5, 0, 60, PGC_USERSET, GUC_NOT_IN_SAMPLE, NULL, NULL, NULL);
 }
-
 
 void _PG_fini(void)
 {
@@ -40,16 +29,23 @@ void _PG_fini(void)
 # define MSG_MORE 0
 #endif
 
-ssize_t sendall(int socket, const char *buffer, size_t length);
-ssize_t sendall(int sockfd, const char *buffer, size_t len)
+ssize_t send_cstring(int socket, const char *buffer);
+ssize_t send_cstring(int sockfd, const char *buffer)
 {
     ssize_t total_bytes = 0;
     ssize_t bytes = 0;
+    uint32_t len = strlen(buffer);
+    uint32_t netlen = htonl(len);
+
+    if (send(sockfd, &netlen, sizeof(uint32_t), MSG_MORE) == -1) {
+        return -1;
+    }
 
     while (len > 0) {
-        bytes = send(sockfd, buffer + total_bytes, len, MSG_MORE);
-        if (bytes == -1)
+        bytes = send(sockfd, buffer + total_bytes, len, 0);
+        if (bytes == -1) {
             break;
+        }
         total_bytes += bytes;
         len -= bytes;
     }
@@ -57,21 +53,40 @@ ssize_t sendall(int sockfd, const char *buffer, size_t len)
     return total_bytes;
 }
 
-ssize_t recvall(int sockfd, char *buffer, size_t len);
-ssize_t recvall(int sockfd, char *buffer, size_t len)
+char * recv_cstring(int sockfd);
+char * recv_cstring(int sockfd)
 {
     ssize_t total_bytes = 0;
     ssize_t bytes = 0;
+    uint32_t len;
+    uint32_t netlen;
+    char * response;
+
+    if (recv(sockfd, &netlen, sizeof(uint32_t), 0)  == -1) {
+        return NULL;
+    }
+
+    len = ntohl(netlen);
+
+    response = malloc(len + 1);
+
+    if (response == NULL) {
+        return NULL;
+    }
+
+    memset(response, 0, len + 1);
 
     while (len > 0) {
-        bytes = recv(sockfd, buffer + total_bytes, len, 0);
-        if (bytes == -1)
-            break;
+        bytes = recv(sockfd, response + total_bytes, len, 0);
+        if (bytes == -1) {
+            free(response);
+            return NULL;
+        }
         total_bytes += bytes;
         len -= bytes;
     }
 
-    return total_bytes;
+    return response;
 }
 
 Datum rpc_request(PG_FUNCTION_ARGS);
@@ -86,27 +101,16 @@ Datum rpc_request(PG_FUNCTION_ARGS)
 
     struct timeval timeout;
 
-    uint32_t send_len;
-    uint32_t send_netlen;
-
-    uint32_t recv_len;
-    uint32_t recv_netlen;
-
     int sockfd;
     struct sockaddr_un addr;
 
     if (PG_ARGISNULL(0)) {
-        elog(ERROR, "An JSON document must be provided");
+        elog(ERROR, "JSON document cannot be NULL");
         PG_RETURN_NULL();
     }
 
     request_text = PG_GETARG_TEXT_P(0);
-
     request = text_to_cstring(request_text);
-
-    send_len = strlen(request);
-
-    send_netlen = htonl(send_len);
 
     sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sockfd == -1) {
@@ -115,6 +119,7 @@ Datum rpc_request(PG_FUNCTION_ARGS)
 
     timeout.tv_sec  = g_timeout;
     timeout.tv_usec = 0;
+
     setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
     setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 
@@ -128,13 +133,8 @@ Datum rpc_request(PG_FUNCTION_ARGS)
         elog(ERROR, "Could not connect: %s", error);
         PG_RETURN_NULL();
     }
-    if (send(sockfd, &send_netlen, sizeof(uint32_t), MSG_MORE) == -1) {
-        error = strerror(errno);
-        elog(ERROR, "Length did not send: %s", error);
-        PG_RETURN_NULL();
-    }
 
-    if (sendall(sockfd, request, send_len) == -1) {
+    if (send_cstring(sockfd, request) == -1) {
         error = strerror(errno);
         elog(ERROR, "Data did not send: %s", error);
         PG_RETURN_NULL();
@@ -146,27 +146,11 @@ Datum rpc_request(PG_FUNCTION_ARGS)
         PG_RETURN_NULL();
     }
 
-    if (recv(sockfd, &recv_netlen, sizeof(uint32_t), 0)  == -1) {
+    response = recv_cstring(sockfd);
+
+    if (response  == NULL) {
         error = strerror(errno);
-        elog(ERROR, "Could not read len: %s", error);
-        PG_RETURN_NULL();
-    }
-
-    recv_len = ntohl(recv_netlen);
-
-    response = malloc(recv_len + 1);
-
-    if (response == NULL) {
-        error = strerror(errno);
-        elog(ERROR, "Could not allocate memory for response: %s", error);
-        PG_RETURN_NULL();
-    }
-
-    memset(response, 0, recv_len + 1);
-
-    if (recvall(sockfd, response, recv_len)  == -1) {
-        free(response);
-        elog(ERROR, "Could not read data");
+        elog(ERROR, "Could not recv data: %s", error);
         PG_RETURN_NULL();
     }
 
